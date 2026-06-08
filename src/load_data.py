4a06d6d909dedcd3042109644e181b4589d0da2e
imdvamshi <imdvamshi@gmail.com>
2026-06-08 12:34:55 +0530

    src code - load_data.py

---
+++ src/load_data.py
@@ -0,0 +1,16 @@
+import sqlite3 as sql
+import pandas as pd
+
+def load_tables():
+    conn = sql.connect('../Data/rideshare.db')
+    tables = {
+    'users' : pd.read_sql_query("SELECT * FROM users",conn),
+    'drivers' : pd.read_sql_query("SELECT * FROM drivers",conn),
+    'riders' : pd.read_sql_query("SELECT * FROM riders",conn),
+    'locations' : pd.read_sql_query("SELECT * FROM locations",conn),
+    'trips' : pd.read_sql_query("SELECT * FROM trips",conn),
+    'payments' : pd.read_sql_query("SELECT * FROM payments",conn),
+    'reviews' : pd.read_sql_query("SELECT * FROM  payments",conn),
+    'cancellations' : pd.read_sql_query("SELECT * FROM cancellations",conn)
+    }
+    return tables
